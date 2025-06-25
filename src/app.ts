45a73ebf89d7a5d03d2b5ec402091cf52c18ba49
SreehariRS <sreeharikannan373@gmail.com>
2025-06-25 15:54:54 +0530

    cors correction

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import ocrRoutes from './routes/ocrRoutes';
 import { connectDB } from './config/db';
+import dotenv from 'dotenv';
+
 
 const app = express();
+dotenv.config()
 
-app.use(cors());
+app.use(cors({
+    origin:process.env.ORIGIN_URI,
+    credentials:true
+}));
 app.use(express.json());
 app.use('/api/ocr', ocrRoutes);
 
